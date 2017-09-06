4d6159fe6b01dc96da68f1477a887559591771cb
Michael McCune <msm@redhat.com>
2017-09-06 16:24:01 -0400

    separate the root path and the sparkpi path
    The root now returns a string that is consistent with the Python and
    Scala examples.

--- src/main/java/io/radanalytics/SparkPiController.java
+++ src/main/java/io/radanalytics/SparkPiController.java
@@ -8,8 +8,12 @@ public class SparkPiController {
 
     @RequestMapping("/")
     public String index() {
+        return "Java Spring Boot SparkPi server running. Add the 'sparkpi' route to this URL to invoke the app.";
+    }
+
+    @RequestMapping("/sparkpi")
+    public String sparkpi() {
         SparkPiProducer pi = new SparkPiProducer();
         return pi.GetPi();
     }
-
 }
