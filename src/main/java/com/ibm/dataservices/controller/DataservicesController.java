f87cf0911d9dc11e72b2ac91acdecdab8efdfe7a
Shailesh Padekar <shailesh.padekar@gmail.com>
2019-12-26 14:27:47 +0530

    f
    f

--- src/main/java/com/ibm/dataservices/controller/DataservicesController.java
+++ src/main/java/com/ibm/dataservices/controller/DataservicesController.java
@@ -1,10 +1,10 @@
 package com.ibm.dataservices.controller;
 
+import org.apache.spark.SparkConf;
 import org.apache.spark.sql.SparkSession;
-import org.apache.log4j.*;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+
+import org.apache.log4j.*;
 
 import com.ibm.featureprocessing.ProcessingFunctions;
 @RestController
@@ -12,11 +12,11 @@ import com.ibm.featureprocessing.ProcessingFunctions;
 public class DataservicesController {
 
     private static final Logger LOGGER = Logger.getRootLogger();
+
    // private static final Logger LOGGER = LoggerFactory.getLogger(DataservicesController.class);
 
     @GetMapping("/getFeatures")
     public String prepareData(@RequestParam("inputJson") String input) {
-
         SparkConf sparkConf = new SparkConf().setAppName("MihuDataserviceAPI");
         sparkConf.setJars(new String[]{"/opt/app-root/src/@project.name@-@project.version@-original.jar"});
         SparkSession spark = SparkSession.builder().config(sparkConf).getOrCreate();
